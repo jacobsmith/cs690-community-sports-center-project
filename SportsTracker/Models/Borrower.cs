fa3f2d097728e6201087e5805169ba835ecef8af
Jacob Smith <jacob.wesley.smith@gmail.com>
2026-05-16 12:25:32 -0400

    can create borrower

--- SportsTracker/Models/Borrower.cs
+++ SportsTracker/Models/Borrower.cs
@@ -1,7 +1,7 @@
 namespace SportsTracker;
 public class Borrower
 {
-    public required int Id { get; set; }
+    public int Id { get; set; }
     public required string Name { get; set; }
     public required string PhoneNumber { get; set; }
 }
--- SportsTracker/Program.cs
+++ SportsTracker/Program.cs
@@ -30,6 +30,25 @@ class Program
             case ("Add Borrower"):
                 {
                     Console.WriteLine("Add Borrower");
+                    var getName = new TextPrompt<string>("Borrower Name: ").Validate(input => input.Length > 2, "[red]Must be at least 2 characters long.[/]");
+                    var name = AnsiConsole.Prompt(getName);
+                    
+                    var getPhoneNumber = new TextPrompt<string>("Phone Number").Validate(input => input.Count(char.IsDigit) == 10, "Must enter a 10 digit phone number");
+                    var phoneNumber = AnsiConsole.Prompt(getPhoneNumber);
+
+                    Console.WriteLine("About to create:");
+                    var newBorrower = new Table();
+                    newBorrower.AddColumn("Name");
+                    newBorrower.AddColumn("Phone Number");
+                    newBorrower.AddRow(name, phoneNumber);
+                    AnsiConsole.Write(newBorrower);
+
+                    if (AnsiConsole.Confirm("Create this Borrower?"))
+                    {
+                        db.Borrower.Add(new Borrower { Name=name, PhoneNumber=phoneNumber });
+                        db.SaveChanges();
+                    }
+
                     break;
                 }
             case ("Add Equipment"):
