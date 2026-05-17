f410055e32abb3c4036a8a3b0493d5bd9ba57b0c
Jacob Smith <jacob.wesley.smith@gmail.com>
2026-05-17 12:19:19 -0400

    refactor out an Equipment UI

---
+++ SportsTracker/EquipmentUI.cs
@@ -0,0 +1,231 @@
+using SportsTracker;
+using Spectre.Console;
+
+class EquipmentUI
+{
+    public static void AddEquipment(AppDbContext db)
+    {
+
+                    Console.WriteLine("Add Equipment");
+
+                    var getName = new TextPrompt<string>("Equipment Name: ").Validate(input => input.Length > 2, "[red]Must be at least 2 characters long.[/]");
+                    var name = AnsiConsole.Prompt(getName);
+                    
+                    // var damagedStatus = AnsiConsole.Prompt(new SelectionPrompt<string>().Title("Current Status").AddChoices("Undamaged", "Damaged"));
+                    // var status = damagedStatus == "Damaged" ? EquipmentStatus.Damaged : EquipmentStatus.Undamaged;
+                    
+                    var getValue = AnsiConsole.Prompt(new TextPrompt<string>("Value of item: ").Validate(input => input.Contains("."), "Must enter dollar amount"));
+                    var value = decimal.Parse(getValue);
+
+                    Console.WriteLine("About to create:");
+                    var newEquipment = new Table();
+                    newEquipment.AddColumn("Name");
+                    newEquipment.AddColumn("Value");
+
+                    newEquipment.AddRow(name, "$" + value.ToString());
+                    AnsiConsole.Write(newEquipment);
+
+                    if (AnsiConsole.Confirm("Create this piece of equipment?"))
+                    {
+                        db.Equipment.Add(new Equipment { Name=name, Status=EquipmentStatus.Undamaged, ValueInDecimal=value, inInventory = true });
+                        db.SaveChanges();
+                    }
+    }
+
+    public static void CheckInEquipment(AppDbContext db)
+    {
+
+                    Console.WriteLine("Check In Equipment");
+
+                    List<Equipment> equipment = db.Equipment.ToList();
+                    var equipmentChoices = new List<string>();
+                    foreach (var item in equipment)
+                    {
+                        if (!item.inInventory) {
+                            equipmentChoices.Add(item.Id + ":" + item.Name);
+                        }
+                    }
+
+
+                    if (equipmentChoices.Count == 0)
+                        {
+                            Console.WriteLine("No equipment is currently checked out.");
+                            AnsiConsole.Markup("Press any key to continue...");
+                            Console.ReadKey();
+                            return;
+                        }
+
+                    var selected = AnsiConsole.Prompt(new MultiSelectionPrompt<string>()
+                    .Title("Select equipment to check in")
+                    .AddChoices(equipmentChoices.ToArray()));
+
+
+                    var selectedEquipmentIds = new List<Int32>();
+                    foreach (var item in selected)
+                    {
+                        selectedEquipmentIds.Add(Int32.Parse(item.Split(":")[0]));
+                    }
+
+                    var selectedEquipment = equipment.FindAll(equipment => selectedEquipmentIds.Contains(equipment.Id));
+
+                    foreach (var item in selectedEquipment)
+                    {
+                        item.inInventory = true;
+                    }
+                    db.SaveChanges();
+    }
+
+    public static void ViewAllEquipment(AppDbContext db)
+    {
+
+                    List<Equipment> equipment = db.Equipment.ToList();
+                    var table = new Table();
+                    table.AddColumn("Name");
+                    table.AddColumn("In Stock");
+
+                    foreach (var item in equipment)
+                        {
+                            table.AddRow(item.Name, item.inInventory.ToString());
+                        }
+
+                    AnsiConsole.Write(table);
+                    AnsiConsole.Markup("Press any key to continue...");
+                    Console.ReadKey();
+    }
+
+    public static void CheckOutEquipment(AppDbContext db)
+    {
+                    Console.WriteLine("Check Out");
+
+                    List<Equipment> equipment = db.Equipment.ToList();
+                    var equipmentChoices = new List<string>();
+                    foreach (var item in equipment)
+                    {
+                        if (item.inInventory && item.Status == EquipmentStatus.Undamaged) {
+                            equipmentChoices.Add(item.Id + ":" + item.Name);
+                        }
+                    }
+
+                    if (equipment.Count == 0)
+                        {
+                            Console.WriteLine("No equipment yet. Add new equipment before checking out.");
+                            AnsiConsole.Markup("Press any key to continue...");
+                            Console.ReadKey();
+                            return;
+                        }
+
+                    var selected = AnsiConsole.Prompt(new MultiSelectionPrompt<string>()
+                    .Title("Select equipment to check out")
+                    .AddChoices(equipmentChoices.ToArray()));
+
+
+                    var selectedEquipmentIds = new List<Int32>();
+                    foreach (var item in selected)
+                    {
+                        selectedEquipmentIds.Add(Int32.Parse(item.Split(":")[0]));
+                    }
+
+                    var selectedEquipment = equipment.FindAll(equipment => selectedEquipmentIds.Contains(equipment.Id));
+
+                    var borrowers = db.Borrower.ToList();
+                    var borrowerChoices = new List<string>();
+                    foreach (var borrower in borrowers)
+                    {
+                        borrowerChoices.Add(borrower.Id.ToString() + ":" + borrower.Name + " (" + borrower.PhoneNumber.ToString() + ")");
+                    }
+                    borrowerChoices.Add("Add New Borrower");
+
+                    var borrowerString = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                    .Title("Select borrower to assign to")
+                    .AddChoices(borrowerChoices));
+
+                    Borrower? selectedBorrower = null;
+                    if (borrowerString == "Add New Borrower")
+                    {
+                        Borrower? maybeBorrower = BorrowerUI.AddBorrower(db);
+
+                        if (maybeBorrower != null)
+                        {
+                            selectedBorrower = maybeBorrower;
+                        }
+                    } else
+                    {
+                        int borrowerId = Int32.Parse(borrowerString.Split(":")[0]);
+                        Borrower? possibleBorrower = db.Borrower.Find(borrowerId);
+
+                        if (possibleBorrower != null)
+                        {
+                            selectedBorrower = possibleBorrower;
+                        }
+                    }
+                    if (selectedBorrower == null)
+        {
+            return;
+        }
+
+                    var today = DateTime.Now;
+
+                    var dateOptions = new List<string>();
+                    for (int i = 0; i < 7; i++)
+                    {
+                        var date = today.AddDays(i);
+                        dateOptions.Add(date.ToLongDateString());
+                    }
+
+
+                    var fromDate = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                    .Title("Select checkout date")
+                    .AddChoices(dateOptions));
+
+                    var hours = new List<string>([
+                        "8:00 am",
+                        "9:00 am",
+                        "10:00 am",
+                        "11:00 am",
+                        "12:00 pm",
+                        "1:00 pm",
+                        "2:00 pm",
+                        "3:00 pm",
+                        "4:00 pm",
+                        "5:00 pm",
+                        "6:00 pm",
+                        "7:00 pm",
+                        "8:00 pm",
+                    ]);
+
+                    var fromTime = AnsiConsole.Prompt(new SelectionPrompt<string>().Title("Checkout Time").AddChoices(hours));
+
+                    // Only show hours after the checkout time
+                    var toHours = new List<string>();
+                    var haveSeenFromTime = false;
+                    for (var i = 0; i < hours.Count; i++)
+                    {
+                        if (fromTime == hours[i])
+                        {
+                            haveSeenFromTime = true;
+                            continue;
+                        }
+
+                        if (haveSeenFromTime)
+                        {
+                            toHours.Add(hours[i]);
+                        }
+                    }
+
+                    var toTime = AnsiConsole.Prompt(new SelectionPrompt<string>().Title("Return Time").AddChoices(toHours));
+
+                    var beginDateTime = DateTime.Parse(fromDate + " " + fromTime);
+                    var endDateTime = DateTime.Parse(fromDate + " " + toTime);
+
+                    foreach (var item in selectedEquipment)
+                    {
+                        var reservation = new Reservation { beginDateTime=beginDateTime, endDateTime=endDateTime, equipment=item, borrower=selectedBorrower };
+                        db.Reservation.Add(reservation);
+                        item.inInventory = false;
+
+                        db.SaveChanges();
+                    }
+
+
+    }
+}
